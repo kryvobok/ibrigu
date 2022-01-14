6023eb8c204dec918b9d93fd301a9a52c1eea2a4
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2022-01-14 20:10:49 +0200

    forms footetrheader

--- archive-stories.php
+++ archive-stories.php
@@ -9,7 +9,7 @@
             <ul class="row stories__list">
                 <?php
                     $args = array(
-                        'numberposts' => 6,
+                        'numberposts' => 9,
                         'post_type' => 'stories',
                         'suppres_filters' => true,
                     );
@@ -38,4 +38,16 @@
             </ul>
         </div>
     </section>
+    <div class="page-blocks">
+              <?php 
+                if ( ! post_password_required() ) :
+                    // Your custom code should here
+                    get_template_part('template-parts/page/content','page');
+                    the_acf_loop();
+                else :
+                  // we will show password form here
+                  echo get_the_password_form();
+                endif;
+              ?>
+            </div>
 <?php get_footer(); ?>
